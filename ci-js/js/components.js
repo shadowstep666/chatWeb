535d79fc65400927f26ad21d03da5531f8398b7a
shadowstep666 <32011122+shadowstep666@users.noreply.github.com>
2019-12-01 17:15:18 +0700

    Add files via upload

---
+++ ci-js/js/components.js
@@ -0,0 +1,196 @@
+const components = {
+  createConversation: `
+    <div id='create-conversation-screen'>
+      <div id='header'>
+        Techkids Chat
+      </div>
+      <div id='create-form-container'>
+        <h2>Create new conversation</h2>
+        <form id='create-conversation-form'>
+          <div class='input-wrapper'>
+            <input
+              class='input'
+              type='text'
+              name='conversationName'
+              placeholder='Conversation name'
+            />
+            <div
+              class='error-message'
+              id='conversation-name-error-message'
+            >
+            </div>
+          </div>
+          <div class='input-wrapper'>
+            <input
+              class='input'
+              type='text'
+              name='friendEmail'
+              placeholder="Your friend's email"
+            />
+            <div
+              class='error-message'
+              id='friend-email-error-message'
+            >
+            </div>
+          </div>
+          <div id='buttons-group'>
+            <input
+              class='button'
+              type='submit'
+              value='Create conversation'
+            />
+            <button
+              id='cancle-create-conversation'
+              class='secondary-button'
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  `,
+  chat: `
+    <div id='chat-screen'>
+      <div id='header'>
+        Techkids Chat
+      </div>
+      <div id='chat-parent'>
+        <div id='conversation-list'>
+          <div id='create-conversation'>
+            <button class='button' id='create-conversation-button'>+ Add conversation</button>
+          </div>
+          <div id='conversation-container'>
+          </div>
+        </div>
+        <div id='chat-container'>
+          <div id='conversation-name'>
+            Techkids Chat
+          </div>
+          <div id='message-container'>
+          </div>
+          <form id='input-message'>
+            <input
+              id='message'
+              type='text'
+              placeholder='Enter your message ...'
+              name='message'
+            />
+            <input type='submit' value='Send' id='submit' />
+          </form>
+        </div>
+      </div>
+    </div>
+  `,
+  message: `
+    <div id='message-content' class='message-content'>
+      <div id='sender' class='sender'></div>
+      <div id='content' class='content'></div>
+    </div>
+  `,
+  index: `
+    <div>
+      <div id='display-name'></div>
+      <div id='email'></div>
+    </div>
+  `,
+  register: `
+    <div id='register-screen'>
+      <div id='register-screen-content'>
+        <div id='logo'>
+          <h1>Techkids Chat</h1>
+        </div>
+        <div id='register-form'>
+          <form id='form-wrapper'>
+            <div id='name-wrapper'>
+              <div class='input-wrapper'>
+                <input
+                  class='input'
+                  type='text'
+                  name='firstName'
+                  placeholder='First name'
+                />
+                <div class='error-message' id='firstName-error-message'></div>
+              </div>
+              <div class='input-wrapper'>
+                <input
+                  class='input'
+                  type='text'
+                  name='lastName'
+                  placeholder='Last name'
+                />
+                <div class='error-message' id='lastName-error-message'></div>
+              </div>
+            </div>
+            <div class='input-wrapper'>
+              <input
+                class='input'
+                type='text'
+                name='email'
+                placeholder='Email address'
+              />
+              <div class='error-message' id='email-error-message'></div>
+            </div>
+            <div class='input-wrapper'>
+              <input
+                class='input'
+                type='password'
+                name='password'
+                placeholder='Password'
+              />
+              <div class='error-message' id='password-error-message'></div>
+            </div>
+            <div class='input-wrapper'>
+              <input
+                class='input'
+                type='password'
+                name='confirmPassword'
+                placeholder='Confirm password'
+              />
+              <div class='error-message' id='confirm-error-message'></div>
+            </div>
+            <div id='form-footer'>
+              <span>Already have an account? Login</span>
+              <input class='button' type='submit' value='Register' />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  `,
+  login: `
+    <div id='login-screen'>
+      <div id='login-screen-content'>
+        <div id='logo'>
+          <h1>Techkids Chat</h1>
+        </div>
+        <div id='login-form'>
+          <form id='form-wrapper'>
+            <div class='input-wrapper'>
+              <input
+                class='input'
+                type='text'
+                name='email'
+                placeholder='Email address'
+              />
+              <div class='error-message' id='email-error-message'></div>
+            </div>
+            <div class='input-wrapper'>
+              <input
+                class='input'
+                type='password'
+                name='password'
+                placeholder='Password'
+              />
+              <div class='error-message' id='password-error-message'></div>
+            </div>
+            <div id='form-footer'>
+              <span id='create-account-button'>Create an account</span>
+              <input class='button' type='submit' value='Log In' />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  `,
+};
---
+++ ci-js/js/view.js
@@ -0,0 +1,190 @@
+const view = {};
+
+view.setMessage = (elementId, message) => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.innerText = message;
+  }
+};
+
+view.sendMessage = (sender, message) => {
+  // render a new message
+  const messageContent = document.createElement('div');
+  messageContent.classList.add('message-content');
+  if (sender) {
+    messageContent.classList.add('friend-message');
+  } else {
+    messageContent.classList.add('my-message');
+  }
+
+  const senderElement = document.createElement('div');
+  senderElement.classList.add('sender');
+  if (sender) {
+    senderElement.innerText = sender;
+  }
+
+  const content = document.createElement('div');
+  content.classList.add('content');
+  content.innerText = message;
+
+  messageContent.appendChild(senderElement);
+  messageContent.appendChild(content);
+
+  const messageContainer = document.getElementById('message-container');
+  if (messageContainer) {
+    messageContainer.appendChild(messageContent);
+  }
+};
+
+view.setActiveScreen = (componentName) => {
+  const app = document.getElementById('app');
+
+  switch (componentName) {
+    case 'chat':
+      if (app) {
+        app.innerHTML = components.chat;
+      }
+
+      // listen submit event
+      const messageForm = document.getElementById('input-message');
+      if (messageForm) {
+        const handleMessageSubmit = (event) => {
+          // get value from input
+          event.preventDefault();
+          const message = messageForm.message.value;
+          
+          // validate messageContent (not null)
+          if (message) {
+            // save message to db
+            model.saveMessage(message);
+
+            // remove old message from input
+            messageForm.message.value = '';
+          }
+        };
+        // addEventListener
+        messageForm.addEventListener('submit', handleMessageSubmit);
+      }
+
+      // load conversation + display old message
+      model.loadConversations();
+
+
+      break;
+
+    case 'index':
+      if (app) {
+        app.innerHTML = components.index;
+        document.getElementById('display-name').innerText = model.logInUser.displayName;
+        document.getElementById('email').innerText = model.logInUser.email;
+      }
+      break;
+  
+    case 'register':
+      if(app) {
+        app.innerHTML = components.register;
+      }
+
+      // Listen submit event
+      const registerForm = document.getElementById('form-wrapper');
+      if (registerForm) {
+        const handleRegisterSubmit = (event) => {
+          event.preventDefault();
+
+          // get input values
+          const firstName = registerForm.firstName.value;
+          const lastName = registerForm.lastName.value;
+          const email = registerForm.email.value;
+          const password = registerForm.password.value;
+          const confirmPassword = registerForm.confirmPassword.value;
+
+          const registerInfo = {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+          };
+
+          controller.validateRegisterForm(registerInfo);
+        };
+        registerForm.addEventListener('submit', handleRegisterSubmit);
+      }
+      break;
+    
+    case 'login':
+      if(app) {
+        app.innerHTML = components.login;
+      }
+
+      const createAccountButton = document.getElementById('create-account-button');
+      if (createAccountButton) {
+        handleCreateAccountClick = (_event) => {
+          view.setActiveScreen('register');
+        };
+        createAccountButton.addEventListener('click', handleCreateAccountClick);
+      }
+
+      // Listen submit event
+      const loginForm = document.getElementById('form-wrapper');
+      if (loginForm) {
+        const handleLoginSubmit = (event) => {
+          event.preventDefault();
+
+          const email = loginForm.email.value;
+          const password = loginForm.password.value;
+
+          const loginInfo = {
+            email,
+            password,
+          };
+
+          controller.validateLoginForm(loginInfo);
+        };
+        loginForm.addEventListener('submit', handleLoginSubmit);
+      }
+      break;
+
+  }
+};
+
+view.renderConversationItem = (conversationInfo) => {
+  const conversationItem = document.createElement('div');
+  conversationItem.innerText = conversationInfo.name;
+  conversationItem.classList.add('conversation-item');
+  conversationItem.id = conversationInfo.id;
+  if (conversationInfo.id === model.activeConversation.id) {
+    conversationItem.classList.add('active-conversation');
+  }
+  changeActiveConversation = (_event) => {
+    // render conversation list
+    const oldConversationItem = document.getElementById(model.activeConversation.id);
+    if (oldConversationItem) {
+      oldConversationItem.classList.remove('active-conversation');
+    }
+    const newConversationItem = document.getElementById(conversationInfo.id);
+    if (newConversationItem) {
+      newConversationItem.classList.add('active-conversation');
+    }
+
+    // update model.activeConversation
+    model.updateActiveConversation(conversationInfo);
+
+    // clear + re-render messages
+    const messageContainer = document.getElementById(`message-container`);
+    messageContainer.innerText = '';
+    model.activeConversation.messages.forEach((message) => {
+      if (message.user === model.logInUser.email) {
+        view.sendMessage('', message.content);
+      } else {
+        view.sendMessage(message.user, message.content);
+      }
+    });
+  };
+  conversationItem.addEventListener('click', changeActiveConversation);
+
+  const conversationContainer = document.getElementById('conversation-container');
+  if (conversationContainer) {
+    conversationContainer.appendChild(conversationItem);
+  }
+};
